048e2ffe04d5c66ff5ca7103317dce13c68f1c03
Bruno Batista <brunobatistaferreirabbf@gmail.com>
2022-09-11 14:27:08 -0300

    "first commit"

---
+++ exercicios/abrirFacebook.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+
+class Principal(webdriver.Chrome):
+    def __init__(self):
+        options = webdriver.ChromeOptions().add_experimental_option('excludeSwitches', ['enable-logging'])
+        service = Service(ChromeDriverManager().install())
+        super(Principal, self).__init__(service=service, options=options)
+        self.maximize_window()
+
+    def abrirFace(self):
+        self.get('https://www.facebook.com')
+        print("O titulo da aplicação é:", self.title)
+        print("O endereço url é:", self.current_url)
+        self.close()
+
+
+
+
+
+bot = Principal()
+bot.abrirFace()
