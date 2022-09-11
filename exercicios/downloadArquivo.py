3ce841ca1850aa37b9d4308b1cb4ee70d4d601bc
Bruno Batista <brunobatistaferreirabbf@gmail.com>
2022-09-11 15:20:08 -0300

    download delay

---
+++ exercicios/downloadArquivo.py
@@ -0,0 +1,20 @@
+from selenium import  webdriver
+from selenium.webdriver.common.by import  By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome()
+
+driver.get("htpps://jquery.com/resources/demos/progressbr/download.html")
+driver.implicitly_wait(5)
+download =  driver.find_element(By.ID, "downloadButton")
+
+download.click()
+WebDriverWait(driver, 30).until(
+    EC.text_to_be_present_in_element((By.CLASS_NAME, 'progress-label'), 'Complete!')
+)
+
+progress_element = driver.find_element(By.CLASS_NAME, "progress-label")
+print(f"{progress_element == 'Complete!'}")
+driver.quit()
